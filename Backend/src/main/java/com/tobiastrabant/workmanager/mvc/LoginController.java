b0cbfaeb21ee53cb7f7d5dc001a6673a5e96a1c5
trabantdev <trabantdeveloper@gmail.com>
2022-05-25 15:52:12 +0200

    added some very basic login

---
+++ Backend/src/main/java/com/tobiastrabant/workmanager/mvc/LoginController.java
@@ -0,0 +1,13 @@
+package com.tobiastrabant.workmanager.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+}
